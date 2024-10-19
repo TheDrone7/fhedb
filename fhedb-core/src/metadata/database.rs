12ef89cfa76b96972f354885fc4443e3a03fb35d
TheDrone7 <h@thedrone7.dev>
2024-10-19 16:48:33 -0400

    Add basic database structure

---
+++ fhedb-core/src/metadata/database.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct Database {
+    pub name: String,
+    pub version: String,
+    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
+    pub created: chrono::DateTime<chrono::Utc>,
+    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
+    pub last_modified: chrono::DateTime<chrono::Utc>,
+    pub document_count: u64,
+    pub size: u64,
+    pub documents: Vec<bson::Document>,
+}
+
+impl Database {
+    pub fn new(name: String) -> Self {
+        Database {
+            name,
+            version: std::env::var("CARGO_PKG_VERSION").unwrap(),
+            created: chrono::Utc::now(),
+            last_modified: chrono::Utc::now(),
+            document_count: 0,
+            size: 0,
+            documents: Vec::new(),
+        }
+    }
+
+    pub fn from(file_contents: &[u8]) -> Result<Self, bson::de::Error> {
+        bson::from_slice(file_contents)
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        bson::to_vec(self).unwrap()
+    }
+}
---
+++ fhedb-core/src/prelude.rs
@@ -0,0 +1,2 @@
+pub use crate::metadata::database::Database;
+pub use bson;
