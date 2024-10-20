3d255dd09e1f98c022d91e4d79485eab135bfc19
TheDrone7 <h@thedrone7.dev>
2024-10-19 20:27:00 -0400

    Remove documents from metadata

--- fhedb-core/src/metadata/database.rs
+++ fhedb-core/src/metadata/database.rs
@@ -9,27 +9,21 @@ pub struct Database {
     #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
     pub last_modified: chrono::DateTime<chrono::Utc>,
     pub document_count: u64,
-    pub offset: u64,
+    pub offsets: Vec<u64>,
     pub size: u64,
-    pub documents: Vec<bson::Document>,
 }
 
 impl Database {
     pub fn new(name: String) -> Self {
-        let mut db = Database {
+        Database {
             name,
             version: std::env::var("CARGO_PKG_VERSION").unwrap(),
             created: chrono::Utc::now(),
             last_modified: chrono::Utc::now(),
             document_count: 0,
-            offset: 0,
             size: 0,
-            documents: Vec::new(),
-        };
-
-        db.fix_offset();
-
-        db
+            offsets: Vec::new(),
+        }
     }
 
     pub fn from(file_contents: &[u8]) -> Result<Self, bson::de::Error> {
@@ -38,24 +32,5 @@ impl Database {
 
     pub fn to_bytes(&self) -> Vec<u8> {
         bson::to_vec(self).unwrap()
-    }
-
-    pub fn fix_offset(&mut self) {
-        self.offset = self.find_offset();
-    }
-
-    fn find_offset(&self) -> u64 {
-        let mut offset = 0;
-        let bytes = self.to_bytes();
-        let documents_bytes = "documents".as_bytes();
-
-        for i in 0..bytes.len() {
-            if bytes[i..i + documents_bytes.len()] == *documents_bytes {
-                offset = i as u64 + documents_bytes.len() as u64 + 5;
-                break;
-            }
-        }
-
-        offset
     }
 }
