f4f606b89175642fe5d570fd917fb7840fb9652e
TheDrone7 <h@thedrone7.dev>
2025-01-22 07:07:23 -0500

    Remove saving filename in metadata

--- fhedb-core/src/metadata/database.rs
+++ fhedb-core/src/metadata/database.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use bson::Document;
 
 /// A struct to hold metadata about the database.
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
@@ -16,7 +17,7 @@ pub struct DbMetadata {
     /// The number of documents in the database.
     pub document_count: u64,
     /// The size of the database in bytes.
-    pub size: u64,
+    pub size: u64
 }
 
 /// Implementation of the DbMetadata struct.
@@ -48,8 +49,8 @@ impl DbMetadata {
 }
 
 /// Implementations of the TryFrom trait for the DbMetadata struct.
-/// This allows the DbMetadata struct to be created from a byte slice.
-impl TryFrom<&[u8]> for DbMetadata {
+/// This allows the DbMetadata struct to be created from a byte slice and a name.
+impl TryFrom<(&[u8], &str)> for DbMetadata {
     /// The error type for the TryFrom trait.
     /// Just forwards the bson error type.
     type Error = bson::de::Error;
@@ -57,7 +58,8 @@ impl TryFrom<&[u8]> for DbMetadata {
     /// Create a new DbMetadata struct from a byte slice.
     ///
     /// # Arguments
-    /// * `file_contents` - The byte slice to create the DbMetadata struct from.
+    /// * `file_data` - A tuple with (The byte slice to create the DbMetadata struct from, The name
+    /// of the database)
     ///
     /// # Returns
     /// A new DbMetadata struct.
@@ -66,11 +68,18 @@ impl TryFrom<&[u8]> for DbMetadata {
     /// ```
     /// use fhedb_core::prelude::DbMetadata;
     /// let bytes: [u8; 4] = [0, 0, 0, 0];
-    /// let db = DbMetadata::try_from(&bytes[..]);
+    /// let db = DbMetadata::try_from((&bytes[..], "test"));
     /// assert_eq!(db.is_err(), true);
     /// ```
-    fn try_from(file_contents: &[u8]) -> Result<Self, Self::Error> {
-        bson::from_slice(file_contents)
+    fn try_from(file_data: (&[u8], &str)) -> Result<Self, Self::Error> {
+        return match Document::from_reader(file_data.0) {
+            Ok(mut contents) => {
+                contents.insert("name", file_data.1);
+                let db: DbMetadata = bson::from_bson(bson::Bson::Document(contents))?;
+                Ok(db)
+            },
+            Err(e) => Err(e),
+        }
     }
 }
 
@@ -81,7 +90,8 @@ impl TryInto<Vec<u8>> for &DbMetadata {
     /// Just forwards the bson error type.
     type Error = bson::ser::Error;
 
-    /// Convert the DbMetadata struct to a byte vector.
+    /// Convert the DbMetadata struct to a byte vector
+    /// after removing the name field.
     ///
     /// # Returns
     /// A byte vector representation of the DbMetadata struct.
@@ -91,9 +101,11 @@ impl TryInto<Vec<u8>> for &DbMetadata {
     /// use fhedb_core::prelude::DbMetadata;
     /// let db = &DbMetadata::new("test".to_owned());
     /// let bytes: Vec<u8> = db.try_into().unwrap();
-    /// assert_eq!(bytes.len(), 117);
+    /// assert_eq!(bytes.len(), 102);
     /// ```
     fn try_into(self) -> Result<Vec<u8>, Self::Error> {
-        bson::to_vec(self)
+        let mut document = bson::to_document(self)?;
+        document.remove("name");
+        bson::to_vec(&document)
     }
 }
