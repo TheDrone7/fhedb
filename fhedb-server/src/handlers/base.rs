bb703aea9e9979f21c1213ba9112ea893d560615
TheDrone7 <h@thedrone7.dev>
2025-12-07 00:43:25 -0500

    Add database query handler

---
+++ fhedb-server/src/handlers/base.rs
@@ -0,0 +1,77 @@
+use fhedb_core::prelude::Database;
+use fhedb_query::ast::DatabaseQuery;
+use log::warn;
+use std::fs::{create_dir_all, remove_dir_all};
+
+use crate::state::ServerState;
+
+pub(crate) fn execute_base_query(
+    query: DatabaseQuery,
+    state: &ServerState,
+) -> Result<String, String> {
+    match query {
+        DatabaseQuery::Create {
+            name,
+            drop_if_exists,
+        } => {
+            if drop_if_exists {
+                drop_db(name.clone(), state)?;
+            }
+            return create_db(name, state);
+        }
+        DatabaseQuery::Drop { name } => return drop_db(name.clone(), &state),
+        DatabaseQuery::List => {
+            let entries = state.data_dir.read_dir().map_err(|e| e.to_string())?;
+            let mut dbs = Vec::new();
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    if entry.file_type().is_ok_and(|e| e.is_dir()) {
+                        if let Ok(db_name) = entry.file_name().into_string() {
+                            dbs.push(db_name);
+                        } else {
+                            warn!("Unable to read directory entry name at base data location.");
+                        }
+                    } else {
+                        warn!("Unknown directory entry at base data location.");
+                    }
+                } else {
+                    warn!("Unable to read directory entry at base data location.");
+                }
+            }
+            return Ok(dbs.join("\n"));
+        }
+    };
+}
+
+fn drop_db(name: String, state: &ServerState) -> Result<String, String> {
+    let mut dbs = state.databases.write().map_err(|e| e.to_string())?;
+    let exists_in_memory = dbs.contains_key(&name);
+    let db_path = state.data_dir.join(&name);
+    let exists_on_disk = db_path.exists();
+
+    if exists_in_memory || exists_on_disk {
+        dbs.remove(&name);
+        if exists_on_disk {
+            remove_dir_all(&db_path).map_err(|e| e.to_string())?;
+        }
+        Ok(format!("Database {} dropped successfully!", name))
+    } else {
+        Err("Database does not exist".to_string())
+    }
+}
+
+fn create_db(name: String, state: &ServerState) -> Result<String, String> {
+    let mut dbs = state.databases.write().map_err(|e| e.to_string())?;
+    let exists_in_memory = dbs.contains_key(&name);
+    let db_path = state.data_dir.join(&name);
+    let exists_on_disk = db_path.exists();
+
+    if exists_in_memory || exists_on_disk {
+        Err("Database already exists".to_string())
+    } else {
+        let db = Database::new(&name, &state.data_dir);
+        create_dir_all(db.path()).map_err(|e| e.to_string())?;
+        dbs.insert(name.clone(), db);
+        Ok(format!("Database {} created successfully!", name))
+    }
+}
--- fhedb-server/src/handlers/mod.rs
+++ fhedb-server/src/handlers/mod.rs
@@ -1,9 +1,26 @@
 mod base;
+pub(crate) mod collection;
+mod contextual;
 
-use axum::extract::Path;
+use crate::{handlers::base::execute_base_query, state::ServerState};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+};
+use fhedb_query::prelude::parse_database_query;
 
-pub async fn handle_base() -> () {
-    ()
+pub async fn handle_base(State(state): State<ServerState>, body: String) -> (StatusCode, String) {
+    let ast_result = parse_database_query(&body);
+    if let Err(errors) = ast_result {
+        let err_strings: Vec<String> = errors.iter().map(|e| e.format(&body)).collect();
+        let err_response = format!("{}", err_strings.join("\n"));
+        return (StatusCode::BAD_REQUEST, err_response);
+    }
+    let ast = ast_result.unwrap();
+    match execute_base_query(ast, &state) {
+        Ok(message) => (StatusCode::OK, message),
+        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err),
+    }
 }
 
 pub async fn handle_db(Path(db_name): Path<String>) -> String {
